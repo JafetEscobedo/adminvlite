95cb3432eb3076868af10e7201b9e8b0fe9e05f7
Jafet Escobedo <jafu.escobedo@gmail.com>
2021-07-04 17:55:48 -0500

    Se actualiza la versión de AdminvLite

--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -7,7 +7,7 @@
   |
   | Versión actual de AdminvLite
  */
-defined('APP_VERSION') || define('APP_VERSION', '1.3.2');
+defined('APP_VERSION') || define('APP_VERSION', '1.3.3');
 
 /*
   | --------------------------------------------------------------------
